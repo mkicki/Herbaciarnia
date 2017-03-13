cd18dff1981623041f646e708a9d903d2935effd
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-03-13 21:38:54 +0100

    fix

--- db/migrate/20170305190920_create_teas.rb
+++ db/migrate/20170305190920_create_teas.rb
@@ -4,7 +4,7 @@ class CreateTeas < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :country
       t.string :kind
-      t.avatar :string
+      t.string :avatar
     end
   end
 end
